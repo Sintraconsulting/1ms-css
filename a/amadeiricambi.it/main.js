dd29f2b5b2b78fc8229dbc1f7d766c90c2b827af
gimmyvd <g.vandeurse@sintraconsulting.eu>
2017-10-13 17:16:51 +0200

    Update main.js

--- a/amadeiricambi.it/main.js
+++ a/amadeiricambi.it/main.js
@@ -1,1 +1,14 @@
-
+(function(){
+  // Divide product name
+  if(document.querySelector('.product-name a')) {
+    let productName = document.querySelectorAll('.product-name a'),
+        secondPart = undefined;
+        
+    for(let i = 0; i < productName.length; i++) {
+      if(productName[i].innerHTML.indexOf('-') == -1) continue;
+      secondPart = productName[i].textContent.split('-').pop();
+      productName[i].innerHTML = productName[i].innerHTML.replace(secondPart, `<span class="secondPart">${secondPart}</span>`);
+    }
+  }
+  
+})()
