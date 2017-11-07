01fcf08df41cc896ba618938072e7f1cf89bf798
gimmyvd <g.vandeurse@sintraconsulting.eu>
2017-11-07 12:43:23 +0100

    Update main.js

--- l/lombardiluciano.it/main.js
+++ l/lombardiluciano.it/main.js
@@ -1,5 +1,5 @@
 (function(){
-  if (!document.querySelector('.oms-edit')) {
+  if (!document.querySelector('.oms-edit') && document.querySelector('.top-image-content')) {
     let topContent = document.querySelector('.top-image-content');
     let topContentBox = topContent.parentNode.parentNode;
     document.querySelector('#top_image').appendChild(topContent);
