b09c67e2832a68df4cedd50b1b83a228366e2845
gimmyvd <g.vandeurse@sintraconsulting.eu>
2017-11-07 12:15:44 +0100

    Create main.js

---
+++ l/lombardiluciano.it/main.js
@@ -0,0 +1,5 @@
+(function(){
+  let topContent = document.querySelector('.top-image-content');
+  let topContentBox = topContent.parentNode.parentNode;
+  document.querySelector('#top_image').appendChild(topContent);
+})();
