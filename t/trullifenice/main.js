34b13c8899890fa3e3b96e50bedf4fe3724b6bb0
gimmyvd <g.vandeurse@sintraconsulting.eu>
2017-12-01 16:11:23 +0100

    Create main.js

---
+++ t/trullifenice/main.js
@@ -0,0 +1,14 @@
+(function(){
+  var products = document.querySelectorAll('.product-item');
+
+  if(products.length){
+    for(var i=0; i < products.length; i++) {
+        var productLink = products[i].querySelector('.product-name a').getAttribute('href');
+        var productButton = document.createElement('a');
+        productButton.classList.add('product-button');
+        productButton.setAttribute('href', productLink);
+        productButton.innerHTML = 'Scopri di più';
+        products[i].querySelector('.product_info').appendChild(productButton);
+      }
+  }
+})();
