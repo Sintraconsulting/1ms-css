9dd2698b694f5e48d50c6b3cc87b3c7f52facb03
gimmyvd <g.vandeurse@sintraconsulting.eu>
2017-12-01 16:54:54 +0100

    Update main.js

--- t/trullifenice/main.js
+++ t/trullifenice/main.js
@@ -5,7 +5,7 @@
     for(var i=0; i < products.length; i++) {
         var productLink = products[i].querySelector('.product-name a').getAttribute('href');
         var productButton = document.createElement('a');
-        productButton.classList.add('product-button');
+        productButton.classList.add('btn-product', 'btn', 'btn-primary');
         productButton.setAttribute('href', productLink);
         productButton.innerHTML = 'Scopri di più';
         products[i].querySelector('.product_info').appendChild(productButton);
