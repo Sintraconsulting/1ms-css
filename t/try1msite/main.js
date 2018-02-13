a8fa636464dd61b83713f35a2fe4d8bc48d486eb
gimmyvd <g.vandeurse@sintraconsulting.eu>
2018-02-13 15:38:51 +0100

    Create main.js

---
+++ t/try1msite/main.js
@@ -0,0 +1,19 @@
+(function() {
+    "use strict";
+
+    if (typeof omsOnInit === 'function') {
+        omsOnInit(doWork);
+    }
+    else {
+        doWork();
+    }
+
+    function doWork() {
+        require(['jquery', 'domReady!'], function($) {
+          $(`<div class="div_central div_ttil omsfloat-100" id="custom-ttil"><div class="row"></div></div>`).insertBefore("#top_image");
+          $("#contact_form_14").insertBefore("#custom-ttil .row");
+          $("#contact_form_14").wrap(`<div class="col-md-6">`);
+        });
+    }
+})();
+
