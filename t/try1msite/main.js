b6a6a77c02bfcb428c4522e592000e2134c16e11
gimmyvd <g.vandeurse@sintraconsulting.eu>
2018-02-13 16:19:04 +0100

    Update main.js

--- t/try1msite/main.js
+++ t/try1msite/main.js
@@ -11,10 +11,11 @@
     function doWork() {
         require(['jquery', 'domReady!'], function($) {
           setTimeout(() => {
-              $(`<div class="div_central div_ttil omsfloat-100" id="custom-ttil"><div class="row"></div></div>`).insertBefore("#top_image");
-              $("#contact_form_14").insertBefore("#custom-ttil .row");
-              $("#contact_form_14").wrap(`<div class="col-md-6">`);
-          }, 100)
+            $(`<div class="div_central div_ttil omsfloat-100" id="custom-ttil"><div class="row"><div class="target"></div></div></div>`).insertBefore("#top_image");
+            $("#contact_form_13").insertBefore("#custom-ttil .target");
+            $("#contact_form_13").wrap(`<div class="col-md-6">`);
+            $(`<div class="col-md-6"><img src="http://css.oneminutesite.eu/t/try1msite/biznestoner-mockup.png" /></div>`).insertBefore("#custom-ttil .col-md-6");
+          }, 300)
         });
     }
 })();
