01acf5c17a295744b92282b015ab2e6a6c9b4145
gimmyvd <g.vandeurse@sintraconsulting.eu>
2018-02-13 15:42:03 +0100

    Update main.js

--- t/try1msite/main.js
+++ t/try1msite/main.js
@@ -10,9 +10,11 @@
 
     function doWork() {
         require(['jquery', 'domReady!'], function($) {
-          $(`<div class="div_central div_ttil omsfloat-100" id="custom-ttil"><div class="row"></div></div>`).insertBefore("#top_image");
-          $("#contact_form_14").insertBefore("#custom-ttil .row");
-          $("#contact_form_14").wrap(`<div class="col-md-6">`);
+          setTimeout(() => {
+              $(`<div class="div_central div_ttil omsfloat-100" id="custom-ttil"><div class="row"></div></div>`).insertBefore("#top_image");
+              $("#contact_form_14").insertBefore("#custom-ttil .row");
+              $("#contact_form_14").wrap(`<div class="col-md-6">`);
+          }, 100)
         });
     }
 })();
